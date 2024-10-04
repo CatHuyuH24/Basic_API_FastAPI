7ba40a582d89f7e25fed534f6c84ef2e3d0fd08a
Ma Cat Huynh <uynhhuc810@gmail.com>
2024-10-04 16:54:45 +0700

    add middleware cors to allow public access of API

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware import cors
+
 from .database import engine
 from . import models
 from .routers import posts, users, auth, votes
@@ -7,11 +9,16 @@ from .routers import posts, users, auth, votes
 # from psycopg2.extras import RealDictCursor
 # import time
 
-
-
-models.Base.metadata.create_all(bind=engine) # create the tables in the database
+models.Base.metadata.create_all(bind=engine) # create the tables in the database using SQLAlchemy
 
 app = FastAPI() # app321 = FastAPI(); technically, app321 is not forbidden when it's about creating a FastAPI instance
+
+origins = ["*"] # anyone can make request to the API
+app.add_middleware(cors.CORSMiddleware, 
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
 
 # RAW SQLDatabase Connection Logic
 # while True:
